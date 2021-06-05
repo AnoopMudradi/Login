59bfd1c058a7398b30f50bdcfe4ea9d60c5429bb
anoop139 <anooprshetty@gmail.com>
2021-06-05 14:50:27 +0530

    added more htmls

---
+++ Om.php
@@ -0,0 +1,28 @@
+<?php
+echo"Om Sai "."<br>";
+//phpinfo();
+$a=0;
+
+$p = array("C", "C++", "Java","Vb","wd");
+/*while($a<count($p))
+{
+	echo$p[$a]." ";
+	$a++;
+}*//*
+foreach($p as $v)
+{
+	echo$v;
+}*/
+function hellophp()
+{
+	echo$a;
+
+}
+//	hellophp()
+$str ="This";
+echo strlen($str);
+
+
+
+
+?>
